bc45e3ec90d5de28aff1bbdea155bcdcbf08130a
MIGUEL  PEREIRA <80460123+Miguelzan@users.noreply.github.com>
2024-05-14 19:55:06 -0300

    Create telas.py

---
+++ telas.py
@@ -0,0 +1,32 @@
+#Importa a classe de criação de telas
+from kivy.uix.screenmanager import Screen
+
+#Criando a homepage
+class Homepage(Screen):
+    pass
+
+#Criando Login
+class Login(Screen):
+    pass
+
+
+class Registro(Screen):
+     pass
+
+
+class Config(Screen):
+    pass
+
+class Tela1(Screen):
+    pass
+
+
+class Informe(Screen):
+    pass
+
+class Equipe_trabalho(Screen):
+    pass
+
+
+class FORMULARIOS(Screen):
+    pass
